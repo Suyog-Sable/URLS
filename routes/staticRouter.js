e064c64f7ff01779b378386bab739676dc4cf763
Suyog-Sable <suyogsable3@gmail.com>
2025-07-01 19:07:25 +0530

    UI Design

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const URL = require('../models/url');   
+const URL = require('../models/url');
 
-router.get('/',async (req,res)=>{
-    if(!req.user) return res.redirect('/login')
-    const allurls = await URL.find({ createdBy: req.user._id});
-    return res.render('home',{
+router.get('/', async (req, res) => {
+    if (!req.user) return res.redirect('/login')
+    const allurls = await URL.find({ createdBy: req.user._id });
+    return res.render('home', {
         urls: allurls,
+        user: req.user,
     });
 })
 
-router.get('/signup',async(req,res)=>{
+router.get('/signup', async (req, res) => {
     return res.render("signup");
 });
 
-router.get('/login',async(req,res)=>{
+router.get('/login', async (req, res) => {
     return res.render("login");
 });
 module.exports = router;
